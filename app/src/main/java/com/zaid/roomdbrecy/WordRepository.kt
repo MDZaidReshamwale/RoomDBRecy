7ccdd0244759483aa091b3d982b457ee7f8a62e7
msre0001 <zaidreshamwale2015@gmail.com>
2021-12-28 17:35:21 +0530

    Recylerview and roomdb

--- app/src/main/java/com/zaid/roomdbrecy/WordRepository.kt
+++ app/src/main/java/com/zaid/roomdbrecy/WordRepository.kt
@@ -1,10 +1,27 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zaid.roomdbrecy
 
 import androidx.annotation.WorkerThread
 import kotlinx.coroutines.flow.Flow
 
-// Declares the DAO as a private property in the constructor. Pass in the DAO
-// instead of the whole database, because you only need access to the DAO
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
 class WordRepository(private val wordDao: WordDao) {
 
     // Room executes all queries on a separate thread.
